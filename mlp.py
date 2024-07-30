a0f6e73eeb924ad8de497c32290f1997d3aeb971
ga111o <ga111o@proton.me>
2024-07-30 17:29:58 +0900

    💩 failed...

--- mlp.py
+++ mlp.py
@@ -11,13 +11,12 @@ def sigmoid_diff(x):
 
 
 def init(
-    inputValue: int = 1,
-    inputValue2: int = 1,
     input_layer_node_num: int = 2,
     hidden_layer_node_num: int = 4,
-    hidden_layer_num: int = 2,
+    hidden_layer_num: int = 1,
     output_layer_node_num: int = 1,
 ):
+
     weight_input2hidden = []
     weight_hidden = []
     weight_hidden2output = []
@@ -26,14 +25,14 @@ def init(
     for _ in range(input_layer_node_num):
         temp = []
         for _ in range(hidden_layer_node_num):
-            temp.append(random.random())
+            temp.append(round(random.random(), 8))
         weight_input2hidden.append(temp)
 
     # weight_hidden2output
     for _ in range(output_layer_node_num):
         temp = []
         for _ in range(hidden_layer_node_num):
-            temp.append(random.random())
+            temp.append(round(random.random(), 8))
         weight_hidden2output.append(temp)
 
     # weight_hidden
@@ -42,24 +41,74 @@ def init(
         for _ in range(hidden_layer_node_num):
             temp = []
             for _ in range(hidden_layer_node_num):
-                temp.append(random.random())
+                temp.append(round(random.random(), 8))
             one_hidden_layer.append(temp)
         weight_hidden.append(one_hidden_layer)
 
     print(
         f"""
-input to hidden
-{weight_input2hidden}
-===============================
-
-hidden
-{weight_hidden}
-===============================
-
-hidden to output
-{weight_hidden2output}
+        | input to hidden
+        | {weight_input2hidden}
+        | ===============================
+        | 
+        | hidden
+        | {weight_hidden}
+        | ===============================
+        | 
+        | hidden to output
+        | {weight_hidden2output}
         """
     )
+    return weight_input2hidden, weight_hidden, weight_hidden2output
 
 
-init()
+def weighted_sum(
+        input_param1: bool,
+        input_param2: bool,
+        weight_input2hidden: list,
+        weight_hidden: list,
+        weight_hidden2output: list,
+        input_layer_node_num: int = 2,
+        hidden_layer_node_num: int = 4,
+        hidden_layer_num: int = 1,
+        output_layer_node_num: int = 1
+):
+
+    input1 = 1 if input_param1 else 0
+    input2 = 1 if input_param2 else 0
+
+    input2hiddenActivated = []
+
+    # 첫 번째 히든레이어 층
+    for i in range(hidden_layer_node_num):
+        weighted = (
+            input1 * weight_input2hidden[0][i]) + (input2 * weight_input2hidden[1][i])
+
+        activated = sigmoid(weighted)
+        input2hiddenActivated.append(activated)
+
+    print(input2hiddenActivated)
+
+    # # 두 번째 이후 히든레이어 층
+
+    # hidden to output
+
+    final_summed = 0
+    for i in range(hidden_layer_node_num):
+        final_summed = weight_hidden2ouput[0][i] * input2hiddenActivated[i]
+
+        print(final_summed)
+
+    y_hat = sigmoid(final_summed)
+
+    return y_hat
+
+
+for i in range(1000):
+
+    for k in range(4):
+
+        weight_input2hidden, weight_hidden, weight_hidden2ouput = init()
+
+        y_hat = weighted_sum(True, True, weight_input2hidden,
+                             weight_hidden, weight_input2hidden)
